02b9376eef364fb58ec23011e5e04fd62ef4cc2e
dev_yo <johan7664@afreecatv.com>
2025-10-10 17:50:15 +0900

    design 모듈 test 의존성 추가

--- library/design/build.gradle.kts
+++ library/design/build.gradle.kts
@@ -11,4 +11,6 @@ dependencies {
     implementation(libs.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
+
+    testImplementation(project(":library:test"))
 }
